dae6c86e04e0fb4d945c26ebfd000d5a225b75f7
Josué Caleb Rodríguez <54115129+WarMachine2019@users.noreply.github.com>
2020-12-09 11:13:42 -0600

    Update DDS.sql

--- Scripts de Base de Datos/DataBaseA/DDS.sql
+++ Scripts de Base de Datos/DataBaseA/DDS.sql
@@ -1,21 +1,29 @@
+/*Sí la base de datos existe la borramos para no tener problemas con la inserción de datos.*/
 DROP DATABASE IF EXISTS BaseA;
+
+/*Creamos la base de datos con la que vamos a trabajar*/
 CREATE DATABASE BaseA CHARACTER SET utf8;
 
+/*Seleccionamos la Base de Datos antes creada para poder manipular los datos mediante consultas.*/
 USE BaseA;
 
+/*Creamos la tabla (Role) para poder asignarle un role a cada usuario que sea ingresado en la tabla Account*/
 DROP TABLE IF EXISTS Role;
 CREATE TABLE Role(
     id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
     txt_roleName CHAR(8) NOT NULL
 );
 
+/*Insertamos los roles correspondientes en la tabla (Role)*/
 INSERT INTO Role ( txt_roleName ) VALUES
     ("ADMIN"),
     ("OPERADOR")
 ;
-
+/*Borramos la table Account en caso de que exista, para no tener errores en la inserción de datos.*/
 DROP TABLE IF EXISTS Account;
 
+/*Creamos la tabla (Account) es donde guardaremos la información de cada usuario, como su nombre y 
+contraseña, ademas le hemos asignado una llave foranea, que hace referencia a la tabla (Role) de esa forma le podes asignar un role. */
 CREATE TABLE Account(
     id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
     txt_name TEXT NOT NULL,
@@ -24,8 +32,13 @@ CREATE TABLE Account(
     CONSTRAINT fk_id_role FOREIGN KEY (id_role) REFERENCES Role(id)
 );
 
+/*Borramos la tabla Config en caso de que exista*/
 DROP TABLE IF EXISTS Config;
 
+/*Creamos la tabla (Config) con el fin de poder guardar la configuración de casa usuario que realizara 
+dibujos, hemos definido varios atributos que se describen en dicha tabla, además hemos 
+definido una llave foranea que hace referencia a la tabla (Account) para poder identificar a 
+cada usuario, para así guardar su configuracion correspondiente, */
 CREATE TABLE Config(
     id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
     txt_penColor CHAR(7) DEFAULT "#000000",
@@ -36,8 +49,12 @@ CREATE TABLE Config(
     CONSTRAINT fk_accountId FOREIGN KEY (accountId) REFERENCES Account(id) 
 );
 
+/*Borramos la tabla Drawing en caso de que exista*/
 DROP TABLE IF EXISTS Drawing;
 
+/*Creamos la tabla (Drawing) con el fin de cuando un usuario cree un dibujo esta pueda guardar 
+el dibujo que realizo el usuario en un formato json, asignandole un nombre de archivo a cada 
+dibujo, además tenemos una llave foranea referenciada a la tabla (Account) para poder identificar a cada usuario */
 CREATE TABLE Drawing(
    id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
    txt_fileName TEXT,
@@ -47,12 +64,15 @@ CREATE TABLE Drawing(
    CONSTRAINT fk_owner_Id FOREIGN KEY (accountId) REFERENCES Account(id)
 );
 
-
+/*Creamos está tabla para poder identificar cada acción que realizan los 
+usarios, por ejemplo, moficar o crear un dibujo.*/
 CREATE TABLE Action(
     id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
     txt_actionName CHAR(15) NOT NULL
 );
 
+/*Insertamos valores en la tabla Action, estos nos indicarán que 
+acción realizo el usuario cuando este dibujando.*/
 INSERT INTO Action ( txt_actionName ) VALUES
     ("VIZUALIZACION"),
     ("MODIFICACION"),
@@ -61,20 +81,26 @@ INSERT INTO Action ( txt_actionName ) VALUES
     ("CREACION")
 ;
 
+/*Creamos esta tabla para poder saber el tipo de elemento que realizo el usuario*/
 CREATE TABLE Element(
     id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
     txt_elementType CHAR(15) NOT NULL
 
 );
 
+/*Insertamos valores en la Tabla (Element) esta nos indicará*/
 INSERT INTO Element ( txt_elementType ) VALUES
     ("DIBUJO"),
     ("CONFIGURACION"),
     ("USUARIO")
 ;
 
+/*Borramos la tabla LogBook en caso de que exista*/
 DROP TABLE IF EXISTS LogBook;
 
+/*Creamos esta tabla (LogBook) que es nuestra bitácora con el fin de registrar todas 
+las acciones que ha realizado el usario, desde el momento que inicio sesión, su 
+configuración y la fecha actual en la que creo o modifico un dibujo.*/
 CREATE TABLE LogBook(
     id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
     tim_recordDate TIMESTAMP NOT NULL,
