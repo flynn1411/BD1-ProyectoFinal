46905d7ccad86c9e073f70eef4db79204e6c8ec8
Josue Izaguirre <flynn1411@gmail.com>
2020-12-10 16:53:28 -0600

    sp dibujo commit

--- Scripts de Base de Datos/DataBaseA/DMS_SP.sql
+++ Scripts de Base de Datos/DataBaseA/DMS_SP.sql
@@ -24,11 +24,6 @@ CREATE PROCEDURE Auth (IN username TEXT,IN accPassword TEXT, OUT userID INT)
        END//
 delimiter ;
 
-CALL Auth("Gabriel", "1234", @userID);
-CALL Auth("Josue", "hola", @userID);
-CALL Auth("Caleb", "0000", @userID);
-SELECT @userID;
-
 delimiter //
 
 /*DROP PROCEDURE GetRole;*/
@@ -38,12 +33,34 @@ CREATE PROCEDURE GetRole (IN username TEXT,IN accPassword TEXT, OUT typeAcc CHAR
          SELECT Role.txt_roleName INTO typeAcc FROM Account JOIN Role ON Account.id_role = Role.id
          WHERE (Account.txt_name = username) AND (Account.txt_password = accPassword) ;
        END//
+
+CREATE PROCEDURE GetDrawingByID (IN drawingID INT, OUT drawing_json JSON)
+      BEGIN
+        DECLARE drawingName TEXT;
+        DECLARE accountID INT;
+        
+        SELECT jso_file INTO drawing_json FROM Drawing WHERE id = drawingID;        
+
+        IF jso_file IS NOT NULL THEN
+
+
+          SELECT Drawing.accountId INTO accountID FROM Drawing WHERE id=drawingID;
+          SELECT Drawing.txt_fileName INTO drawingName FROM Drawing WHERE id=drawingID;
+
+          INSERT INTO LogBook(accountId, actionId, elementId, txt_elementName, tim_recordDate) VALUES(
+                  accountID,
+                  1,
+                  1,
+                  drawingName,
+                  NOW()
+                );
+
+        END IF;
+        
+        COMMIT;
+      END//
+
 delimiter ; 
-
-CALL GetRole("Gabriel", "1234", @typeAcc);
-
-SELECT @typeAcc;
-
 /*
 Mirar TODOS los Procedimientos almacenados 
 
